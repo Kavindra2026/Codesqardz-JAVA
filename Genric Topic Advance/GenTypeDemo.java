577c73ef6766469abf8ad4a2d56d6989cb4052b2
Kavindra Singh <kavindrasingh7535@gmail.com>
2025-08-28 13:20:22 +0530

    Genric  class

---
+++ Genric Topic Advance/GenTypeDemo.java
@@ -0,0 +1,19 @@
+class GenStr<T extends String> {
+  T str; // here, T will be replaced by String
+
+  GenStr(T o) {
+    str = o;
+  }
+
+  T getstr() {
+    return str;
+  }
+}
+
+public class GenTypeDemo {
+  public static void main(String args[]) {
+    GenStr<String> fOb = new GenStr<String>("aaa");
+    System.out.println(fOb.getstr());
+  }
+
+}
